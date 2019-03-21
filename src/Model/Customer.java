4cd70136b857c52d78714e77389b8edfc17e1217
Austin Smith <awsmith815@yahoo.com>
2019-03-21 12:21:41 -0400

    Create
    Build customer and customer controllers

--- src/Model/Customer.java
+++ src/Model/Customer.java
@@ -19,6 +19,77 @@ public class Customer {
     }
     
     //Getters
-    
-    
+    public int getCustomerID(){
+        return customerID;
+    }
+    public int getAddressID(){
+        return addressID;
+    }
+    public int getCityID(){
+        return cityID;
+    }
+    public int getCountryID(){
+        return countryID;
+    }
+    public int getActive(){
+        return active;
+    }
+    public String getCustomerName(){
+        return customerName;
+    }
+    public String getPhone(){
+        return phone;
+    }
+    public String getAddress1(){
+        return address1;
+    }
+    public String getAddress2(){
+        return address2;
+    }
+    public String getPostalCode(){
+        return postalCode;
+    }
+    public String getCity(){
+        return city;
+    }
+    public String getCountry(){
+        return country;
+    }
+    //Setters
+    public void setCustomerID(int customerID){
+        this.customerID=customerID;
+    }
+    public void setAddressID(int addressID){
+        this.addressID=addressID;
+    }
+    public void setCityID(int cityID){
+        this.cityID=cityID;
+    }
+    public void setCountryID(int countryID){
+        this.countryID=countryID;
+    }
+    public void setActive(int active){
+        this.active=active;
+    }
+    public void setCustomerName(String customerName){
+        this.customerName=customerName;
+    }
+    public void setPhone(String phone){
+        this.phone=phone;
+    }
+    public void setAddress1(String address1){
+        this.address1=address1;
+    }
+    public void setAddress2(String address2){
+        this.address2=address2;
+    }
+    public void setPostalCode(String postalCode){
+        this.postalCode=postalCode;
+    }
+    public void setCity(String city){
+        this.city=city;
+    }
+    public void setCountry(String country){
+        this.country=country;
+    }
 }
